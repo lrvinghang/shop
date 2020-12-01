5e944c5794f97adeba0c8fdb1ccb95b1a7b09e03
lrvinghang <Qq1157026884>
2020-12-01 21:41:34 +0800

    feat: SpringBoot框架 shop商场项目

---
+++ shop/shop-common/src/main/java/com/lrving/App.java
@@ -0,0 +1,13 @@
+package com.lrving;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
